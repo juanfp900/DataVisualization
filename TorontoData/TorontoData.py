dbd07d819841231f38a45bfb2251a1fab99ad32c
Juan Pulido <juanfp900@gmail.com>
2020-05-04 18:40:15 -0600

    Updated .py file

--- TorontoData/TorontoData.py
+++ TorontoData/TorontoData.py
@@ -1,7 +1,3 @@
-#-------- MAY NEED TO DO THE FOLLOWING:
-#-------- pip install descartes 
-#-------- OR
-#-------- conda install -c conda-forge descartes
 
 import pandas as pd
 import geopandas as gpd
@@ -9,52 +5,90 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-class TorontoData:
 
-    def organizeNeighbourhoodData(self):
-        dataFile = pd.read_csv('neighbourhood-profiles-2016.csv')
-        dataFile.drop(dataFile.columns[[0, 1, 3]],inplace=True, axis=1)
-        dataFile.set_index('Topic',inplace=True) #set index before transponse.
-        df = dataFile.T  #Transpose   
-        df.reset_index(inplace=True) #reset index of dataframe
-        for col in df.columns:
+class TorontoData:
+    years = ['2011', '2006', '2001'] #tabs that appear in excel file
+    
+    def OrgainzeOldData(self):
+        
+        dfList = []
+        global years
+        for year in years:
+            oldData = pd.read_excel('neighbourhood-data-2001-2011.xlsx', sheet_name=year)
+            oldData.drop(oldData.columns[[0]], inplace = True, axis = 1)
+            oldData.set_index('Topic',inplace=True)
+            oldData.columns = [col.strip() for col in oldData.columns]
+            dfOld = oldData.T  #Transpose   
+            dfOld.reset_index(inplace=True) #reset index of dataframe
+            
+            dfOld.rename(columns={'index': 'Neighbourhood'},inplace = True) #rename column header
+            dfOld.drop(dfOld.columns.difference(['Income of households', 'Neighbourhood']), 1, inplace=True)
+            
+            #drops top row
+            dfOld.columns = dfOld.iloc[0] 
+            dfOld = dfOld.reindex(dfOld.index.drop(0)).reset_index(drop=True)
+            
+            #removes white spaces from column headers
+            dfOld.rename(columns=lambda x: x.strip(), inplace=True)
+           
+            dfOld.rename(columns={'Attribute': 'Neighbourhood'},inplace = True) #rename column header
+            neighbourhoodCol = dfOld[['Neighbourhood']].copy()
+          
+            if year == '2001':
+                dfOld = dfOld.loc[:,'Census family income in 2000 of all families - 20% Sample Data':'$100,000 and over']
+            
+            if year == '2006':
+                dfOld = dfOld.loc[:,'After-tax income in 2005 of private households - 20% sample data':'$100,000 and over']
+                
+            if year == '2011':
+                dfOld = dfOld.loc[:,'After-tax income of households in 2010 of private households':'$125,000 and over']
+                dfOld.drop(['$100,000 and over'], inplace = True, axis = 1)
+              
+            dfList.append(dfOld)
+        return(dfList, neighbourhoodCol)
+    
+    
+  
+    def Organize2016Data(self):
+        newData = pd.read_csv('neighbourhood-profiles-2016.csv')
+        newData.drop(newData.columns[[0, 1, 3]],inplace=True, axis=1)
+        newData.set_index('Topic',inplace=True) #set index before transponse.
+        dfNew = newData.T  #Transpose   
+        dfNew.reset_index(inplace=True) #reset index of dataframe
+        for col in dfNew.columns:
             try:
                 col.strip()
             except:
                 print(col)
-        
-        df.rename(columns={'index': 'Neighbourhood'},inplace = True) #rename column header
-        df.drop(df.columns.difference(['Income of households in 2015', 'Neighbourhood']), 1, inplace=True)
-        df.columns = df.iloc[0] #these next 3 lines remove top row
-        df = df.reindex(df.index.drop(0)).reset_index(drop=True)
-        df.columns.name = None 
-        df.rename(columns={'Characteristic' : 'Neighbourhood'}, inplace=True)
+        dfNew.rename(columns={'index': 'Neighbourhood'},inplace = True) #rename column header
+        dfNew.drop(dfNew.columns.difference(['Income of households in 2015', 'Neighbourhood']), 1, inplace=True)
+        dfNew.columns = dfNew.iloc[0] #these next 3 lines remove top row
+        dfNew = dfNew.reindex(dfNew.index.drop(0)).reset_index(drop=True)
+        dfNew.columns.name = None 
+        dfNew.rename(columns={'Characteristic' : 'Neighbourhood'}, inplace=True)
     
-        neighbourhoodCol = df[['Neighbourhood']].copy()
-        updated_df = df.loc[:,'Total - Household after-tax income groups in 2015 for private households - 100% data':]
+        neighbourhoodCol = dfNew[['Neighbourhood']].copy()
+        dfNew = dfNew.loc[:,'Total - Household after-tax income groups in 2015 for private households - 100% data':]
         
         
-        return (updated_df, neighbourhoodCol)
-       
-        
+        return (dfNew, neighbourhoodCol)
     
-    def readShapeData(self):
-        shapeFile = 'Neighbourhoods.shp'
-        shape_df = gpd.read_file(shapeFile)
-        
-        shape_df['Neighbourhood'] = shape_df['FIELD_7'].str.replace(r"\(.*\)","") 
-        shape_df['Neighbourhood'] = shape_df['Neighbourhood'].str.strip()
-       
     
-        return(shape_df)
+    def ListToDataFrame(self, dfList, neighbourhoodCol):
         
-    
+        incomeList = []
+        
+        for dataframe in dfList:
+            oldIncome = self.CalculateMedianIncome(dataframe,neighbourhoodCol)
+            incomeList.append(oldIncome)
+      
+        return(incomeList)
+  
 
-    def calculateMedianIncome(self, updated_df, neighbourhoodCol):
+    def CalculateMedianIncome(self, dataFrame, neighbourhoodCol):
         
-        string_df = updated_df.replace(',','', regex=True) #remove commas from strings
+        string_df = dataFrame.replace(',','', regex=True) #remove commas from strings
         float_df = string_df.astype(float) #convert strings to floats for computation
-         #float_df.columns = [col.strip() for col in float_df.columns]
         float_df = pd.concat([neighbourhoodCol,  float_df], axis=1)
         float_df.columns = [col.strip() for col in float_df.columns]
         
@@ -82,55 +116,110 @@ class TorontoData:
                 totalPercentage = 0
             
             columnIndex = columnIndex + 1
+
+       # colname = df.columns[pos]
         
-        float_df['Median Income'] = incomeList
+        if dataFrame.columns[0] == 'Census family income in 2000 of all families - 20% Sample Data':
+            float_df['Median Income 2000'] = incomeList
+            calculated_df = float_df[['Neighbourhood','Median Income 2000']] 
         
-        calculated_df = float_df[['Neighbourhood','Median Income']] 
-      
+        if dataFrame.columns[0] == 'After-tax income in 2005 of private households - 20% sample data':
+            float_df['Median Income 2005'] = incomeList
+            calculated_df = float_df[['Neighbourhood','Median Income 2005']] 
+        
+        if dataFrame.columns[0] == 'After-tax income of households in 2010 of private households':
+            float_df['Median Income 2010'] = incomeList
+            calculated_df = float_df[['Neighbourhood','Median Income 2010']] 
+        
+        if dataFrame.columns[0] == 'Total - Household after-tax income groups in 2015 for private households - 100% data':
+            float_df['Median Income 2015'] = incomeList
+            calculated_df = float_df[['Neighbourhood','Median Income 2015']] 
+        
+     
         calculated_df.columns = [col.strip() for col in calculated_df.columns]
+  
 
         return(calculated_df)
         
 
+        
+    def ReadShapeData(self):
+        shapeFile = '/Users/Juanp/Desktop/PythonProjects/DataVisualization/TorontoData/Neighbourhoods/Neighbourhoods.shp'
+        shape_df = gpd.read_file(shapeFile)
+        
+        shape_df['Neighbourhood'] = shape_df['FIELD_7'].str.replace(r"\(.*\)","") 
+        shape_df['Neighbourhood'] = shape_df['Neighbourhood'].str.strip()
+       
+    
+        return(shape_df)
+        
+    
+    def SendToShapeFile(self, shape_df, incomeList):
+        i=0
+        for income in incomeList:
+           if i==0:
+              temp1 = self.Merge(shape_df, income)
+           if i==1:
+              temp2 = self.Merge(temp1, income)
+           if i==2:
+              merged_shapeFile = self.Merge(temp2, income)
+           i=i+1
+          
+        return(merged_shapeFile)
+       
+  
 
-    def merge(self,shape_df, calculated_df):
-        merged_left = pd.merge(left=shape_df, right=calculated_df, how='left', 
+    def Merge(self,shape_df, income):
+        merged_left = pd.merge(left=shape_df, right=income, how='left', 
                                left_on='Neighbourhood', right_on='Neighbourhood')
+        merged_left.fillna(0)
         return(merged_left)
+        
+       
     
-    
-
-    def plotData(self, merged_left, shape_df):
+    def CreateShapeFile(self, mergedFinal):
+        mergedFinal.columns = [col.strip() for col in mergedFinal.columns]
         
-        fig, ax = plt.subplots(1, figsize=(60, 40)) #need a fig to create a title. Can hide
-        ax.axis('off') #Hide uneecessary axis we only care about title right now
-        ax.set_title('Household After-Tax Income by Neighbourhood - 2015', fontdict={'fontsize': '40'})
         
-        color = 'Greens'
-
-        shape_df.plot('Neighbourhood', cmap=color, linewidth=2, ax=ax, edgecolor='0.8', figsize=(160,80))
+        gdf = gpd.GeoDataFrame(mergedFinal, geometry='geometry')
+        gdf.to_file('TorontoIncomeData.shp', driver='ESRI Shapefile')
         
-        for index, row in merged_left.iterrows():
-            ann = '         {}\n'.format(row) #add a space to prevent overlaps
-            plt.annotate(row['Neighbourhood'], xy=(row['FIELD_11'], row['FIELD_12']),
-            horizontalalignment='center', fontsize='small', color='black')
+        
+    def Printall(self, mergedPrint): 
+        new = mergedPrint['Median Income 2000'].unique().toList()
+        
+        
+        print("year 2000: " + str(mergedPrint['Median Income 2000'].unique()))
+        print("year 2005: " + str(mergedPrint['Median Income 2005'].unique()))
+        print("year 2010: " + str(mergedPrint['Median Income 2010'].unique()))
+        print("year 2015: " + str(mergedPrint['Median Income 2015'].unique()))
+        
+        
 
-        plt.show()
- 
        
 def main():
     sns.set(style="darkgrid") 
     obj = TorontoData() #instantiate class
-    updated_df, neighbourhoodCol = obj.organizeNeighbourhoodData()
     
-    calculated_df =  obj.calculateMedianIncome(updated_df, neighbourhoodCol)
-    shape_df = obj.readShapeData()
-
-    merged_left  = obj.merge(calculated_df,shape_df)
     
-    obj.plotData(merged_left, shape_df)
+    dfList, neighbourhoodCol = obj.OrgainzeOldData()
+    #This method calls the CalculateMedianIncome function inside it. Returns a list of dataframes
+    incomeList = obj.ListToDataFrame(dfList, neighbourhoodCol)
+    shape_df = obj.ReadShapeData()
+    mergedShapeFile = obj.SendToShapeFile(shape_df, incomeList)
 
- 
+    dfNew, neighbourhoodCol = obj.Organize2016Data()
+    incomeNew =  obj.CalculateMedianIncome(dfNew, neighbourhoodCol)
+    
+    
+    mergedFinal = obj.Merge(mergedShapeFile, incomeNew)
+    
+    mergedPrint = obj.CreateShapeFile(mergedFinal)
+    
+    #obj.Printall(mergedFinal)
+    #obj
+    
+
 main()
 
 
